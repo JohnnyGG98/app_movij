6221dc2e015a823686bedad5a37d895a68952e5c
Johnny Garcia <johnnygar98@hotmail.com>
2020-10-31 22:47:16 -0500

    ADD: Ordena Game
    Add result example, and check the with a circle when is correct
    
    closes #15

--- lib/pages/juegos/fisica/ordena/ordena_main.dart
+++ lib/pages/juegos/fisica/ordena/ordena_main.dart
@@ -3,11 +3,10 @@ import 'dart:math';
 import 'package:app_movij/config/config_export.dart';
 import 'package:app_movij/pages/juegos/fisica/encuentra/personaje_encuentra.dart';
 import 'package:app_movij/templates/widgets/widget_victoria.dart';
-import 'package:app_movij/utils/speak.dart';
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 
 class OrdenaMainPage extends StatefulWidget {
-
   final String _img;
 
   OrdenaMainPage(this._img);
@@ -16,109 +15,151 @@ class OrdenaMainPage extends StatefulWidget {
   _OrdenaMainPageState createState() => _OrdenaMainPageState();
 }
 
-// Constantes de tamaños 
+// Constantes de tamaños
 const double _GRANDE = 150;
 const double _MEDIANA = 125;
 const double _PEQUENA = 100;
-// TODO: Cambiar a un matamo dinamico dependiendo del size del dispositivo 
+// TODO: Cambiar a un matamo dinamico dependiendo del size del dispositivo
 const double _SIZECTN = 175;
 
 class _OrdenaMainPageState extends State<OrdenaMainPage> {
-
   int _seed = rand.nextInt(10);
   int _gameMode = rand.nextInt(2);
   int _intentos = 1;
-  bool _win = false; 
-  bool _mostrarVictoria = false; 
+  bool _win = false;
+  bool _mostrarVictoria = false;
   String _orden = '';
   List<double> _tamanos;
-  int _numMov = 0; 
+  List<double> ordenados;
+  int _numMov = 0;
 
   @override
   Widget build(BuildContext context) {
     if (_tamanos == null) {
       _tamanos = _getTamanos();
+      ordenados = getOrdenados();
     }
 
     if (_intentos == 1 && _numMov == 0) {
-      _youWin();
-      if (_win ) {
-        _newGame();
-        _win = false;
-      }  
+      validarGame();
     }
 
     if (_gameMode == 0) {
-      _orden = 'Pequeno a grande';
+      _orden = 'Pequeño a grande';
     } else {
-      _orden = 'Grande a pequeno';
+      _orden = 'Grande a pequeño';
     }
 
     if (_win) {
       _win = false;
-      speakNow('Ganaste... Felicidades!');
-      Future.delayed(const Duration(milliseconds: 2000), () {
+      // speakNow('Ganaste... Felicidades!');
+      Future.delayed(const Duration(milliseconds: 1000), () {
         setState(() {
-          _mostrarVictoria = true; 
+          _mostrarVictoria = true;
         });
       });
     }
-    
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Número movimientos:  $_numMov'),
+        title: Text('$_orden # $_numMov'),
       ),
-
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.check),
         onPressed: _mostrarVictoria ? _newGame : null,
-        backgroundColor: _mostrarVictoria ? AppThemeColors.GREEN : Colors.black45,
+        backgroundColor: _mostrarVictoria
+            ? AppThemeColors.GREEN
+            : AppThemeColors.WHITE_SHADOW,
       ),
-
-      body: _mostrarVictoria ? VictoriaJuego('Edisson') : Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: 30.0,
-            width: double.infinity,
-            child: Center(
-              child: Text(_win ? 'YOU WIN' : _orden,
-                style: TextStyle(
-                  fontSize: 20.0
-                ), 
-              ),
-            ),
-            margin: EdgeInsets.symmetric(vertical: 10.0),
-          ),
-
-          Expanded(
-            child: _game()
-          )
-        ],
-      ),
-
+      body: _mostrarVictoria ? VictoriaJuego('Edisson') : _game(),
     );
   }
 
   void _newGame() {
-    setState(() {
-      _numMov = 0;
-      _mostrarVictoria = false;
-      _seed++;
-      _intentos++;
-      _tamanos = _getTamanos();
-      _gameMode = rand.nextInt(2);
-    });
+    if (_win) {
+      validarGame();
+    } else {
+      setState(() {
+        _numMov = 0;
+        _mostrarVictoria = false;
+        _seed++;
+        _intentos++;
+        _tamanos = _getTamanos();
+        ordenados = getOrdenados();
+        _gameMode = rand.nextInt(2);
+      });
+    }
   }
 
   Widget _game() {
+    return Stack(
+      children: [
+        listReordenable(),
+        Positioned(
+          right: 20,
+          top: 20,
+          child: resultExample(),
+        ),
+      ],
+    );
+  }
+
+  Widget resultExample() {
+    return Container(
+      width: 75,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: AppThemeColors.WHITE_SHADOW,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        children: ordenados.map(
+          (s) {
+            final Widget img = Container(
+              height: 75,
+              child: _getImg(s * 0.25),
+            );
+            final int index = ordenados.indexOf(s);
+            final bool isSelected = _tamanos[index] == s;
+            if (isSelected) {
+              Flame.audio.play('correct.wav');
+              return Stack(
+                children: [
+                  Positioned(
+                    left: 10,
+                    top: 10,
+                    right: 10,
+                    bottom: 10,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppThemeColors.GREEN.withOpacity(0.85),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  img,
+                ],
+              );
+            }
+            return img;
+          },
+        ).toList(),
+      ),
+    );
+  }
+
+  Widget listReordenable() {
     return ReorderableListView(
-      children: _tamanos.map((s) => Container(
-        key: ValueKey(_seed + s),
-        height: _SIZECTN,
-        width: double.infinity,
-        child: _getImg(s),
-      )).toList(),
+      padding: EdgeInsets.zero,
+      children: _tamanos
+          .map((s) => Container(
+                key: ValueKey(_seed + s),
+                height: _SIZECTN,
+                width: double.infinity,
+                child: _getImg(s),
+              ))
+          .toList(),
       onReorder: (oldIndex, newIndex) {
         setState(() {
           _updateMyItems(oldIndex, newIndex);
@@ -140,42 +181,51 @@ class _OrdenaMainPageState extends State<OrdenaMainPage> {
     return Center(
       child: Container(
         height: tamano,
-        width: tamano,
         child: Image(
           image: AssetImage(widget._img),
           fit: BoxFit.contain,
         ),
       ),
-    ); 
+    );
   }
 
-  _updateMyItems(int oldIndex,int newIndex) {
+  _updateMyItems(int oldIndex, int newIndex) {
     _seed++;
     if (newIndex > oldIndex) {
       newIndex -= 1;
     }
 
     double aux = _tamanos.removeAt(oldIndex);
-    _tamanos.insert(newIndex, aux);    
+    _tamanos.insert(newIndex, aux);
     _youWin();
-    _numMov++; 
+    _numMov++;
   }
 
   void _youWin() {
+    _win = true;
+    for (var i = 0; i < ordenados.length; i++) {
+      if (_tamanos[i] != ordenados[i]) {
+        _win = false;
+        break;
+      }
+    }
+  }
+
+  void validarGame() {
+    _youWin();
+    if (_win) {
+      _win = false;
+      _newGame();
+    }
+  }
+
+  List<double> getOrdenados() {
     List<double> ordenados = List.of(_tamanos);
     if (_gameMode == 0) {
       ordenados.sort();
     } else {
       ordenados.sort((a, b) => b.compareTo(a));
     }
-    
-    _win = true; 
-    for (var i = 0; i < ordenados.length; i++) {
-      if (_tamanos[i] != ordenados[i]) {
-        _win = false;
-        break; 
-      }
-    }
+    return ordenados;
   }
-
 }
